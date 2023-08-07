52b5e0d46719a0ae0db9edfa6f8f66da7fe3b7c7
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-08-07 19:14:43 +0100

    Updates

--- scrabble.js
+++ scrabble.js
@@ -1,30 +1,12 @@
-const lettersToCounts = new Map([
-  ["a", 9],
-  ["b", 2],
-  ["c", 2],
-  ["d", 4],
-  ["e", 12],
-  ["f", 2],
-  ["g", 3],
-  ["h", 2],
-  ["i", 9],
-  ["j", 1],
-  ["k", 1],
-  ["l", 4],
-  ["m", 2],
-  ["n", 6],
-  ["o", 8],
-  ["p", 2],
-  ["q", 1],
-  ["r", 6],
-  ["s", 4],
-  ["t", 6],
-  ["u", 4],
-  ["v", 2],
-  ["w", 2],
-  ["x", 1],
-  ["y", 2],
-  ["z", 1],
+const countsToLetters = new Map([
+  [1, "jkqxz"],
+  [2, "bcfhmpvwy"],
+  [3, "g"],
+  [4, "dlsu"],
+  [6, "nrt"],
+  [8, "o"],
+  [9, "ai"],
+  [12, "e"],
 ]);
 
 const valuesToLetters = new Map([
@@ -45,8 +27,10 @@ const lettersToValues = new Map(
 );
 
 const getFullSetOfScrabbleLetters = () => {
-  return Array.from(lettersToCounts.entries())
-    .flatMap(([letter, count]) => Array(count).fill(letter));
+  return Array.from(countsToLetters.entries())
+    .flatMap(([count, letters]) =>
+      Array.from(letters).flatMap((letter) =>
+        Array(count).fill(letter)));
 };
 
 const getScrabbleScore = (word) => {
